17074b510972ed2815db09a32bd127f49d22b21f
João Félix <132092155+joaofelixss@users.noreply.github.com>
2025-05-03 14:39:15 -0400

    Delete js directory

--- js/index.js
+++
@@ -1,9 +0,0 @@
-import initAnimacaoScroll from './modules/scroll-animacao.js';
-import navBar from './modules/scroll-navBar.js';
-import navBarScroll from './modules/btn-mobile.js';
-import initTabNav from './modules/initTabnav.js';
-
-initAnimacaoScroll();
-navBar();
-navBarScroll();
-initTabNav();
